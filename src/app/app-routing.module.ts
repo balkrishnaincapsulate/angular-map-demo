5fc85a4c78fbc033de4eff0ab4ead0ec4a704437
Balkrishna <bbhatt@incapsulate.com>
2019-12-09 23:38:40 +0530

    Routing final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MapComponentComponent} from './map-component/map-component.component';
+import { SearchMapComponent } from './search-map/search-map.component';
 
-const routes: Routes = [];
+const route : Routes = [
+    {path: '', component: SearchMapComponent},
+    {path: 'map', component: MapComponentComponent},    
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports:[RouterModule.forRoot(route)],
+    exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
